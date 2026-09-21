45e5594010b39ad5fe8062fe7335cf91da899e0b
Affan Khan <khanaffan070@gmail.com>
2026-09-21 15:35:57 +0530

    brand: replace RolePilot icon with uploaded mark

--- app/components/brand.tsx
+++ app/components/brand.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Brand({href="/"}:{href?:string}){
   return <Link href={href} className="brand-lockup" aria-label="RolePilot home">
-    <Image src="/rolepilot-mark.svg" alt="" width={38} height={38} priority className="brand-mark"/>
+    <Image src="/rolepilot-mark.png" alt="" width={38} height={38} priority className="brand-mark"/>
     <span className="brand-wordmark">Role<span>Pilot</span></span>
   </Link>;
 }
