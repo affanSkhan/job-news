f3b5d83070743d46d5da7de53f1c0976ea414dc3
Affan Khan <khanaffan070@gmail.com>
2026-09-25 02:24:49 +0530

    fix: remove unsupported robots host directive

--- app/robots.ts
+++ app/robots.ts
@@ -8,7 +8,6 @@ export default function robots():MetadataRoute.Robots{
       allow:"/",
       disallow:["/api/","/admin","/account","/auth"]
     },
-    sitemap:SITE_URL+"/sitemap.xml",
-    host:SITE_URL
+    sitemap:SITE_URL+"/sitemap.xml"
   };
 }
